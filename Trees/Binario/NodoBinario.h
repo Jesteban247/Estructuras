251796dc130d27d59045d522cfbc91087b7045b3
XDavidF29 <140862157+XDavidF29@users.noreply.github.com>
2023-09-15 12:21:05 -0500

    NodoBinario.h

--- Trees/Binario/NodoBinario.h
+++ Trees/Binario/NodoBinario.h
@@ -7,19 +7,38 @@ using namespace std;
 template <class T>
 class NodoBinario
 {
-    protected:
-    T dato;
-    NodoBinario<T> *hijoIzq;
-    NodoBinario<T> *hijoDer;
+protected:
+    T dato;                      // Dato almacenado en el nodo
+    NodoBinario<T> *hijoIzq;    // Puntero al hijo izquierdo
+    NodoBinario<T> *hijoDer;    // Puntero al hijo derecho
 
-    public:
+public:
+    // Constructor
     NodoBinario();
+
+    // Destructor
     ~NodoBinario();
+
+    // Métodos para establecer y obtener el dato del nodo
+
+    //  Establece el valor de dato en el nodo.
     void fijarDato(T& dato);
+
+    //  Obtiene el valor almacenado en el nodo.
     T& obtenerDato();
+
+    // Métodos para establecer y obtener los hijos del nodo
+
+    //  Obtiene el puntero al hijo izquierdo del nodo.
     NodoBinario<T>* obtenerHijoIzq();
+
+    //  Obtiene el puntero al hijo derecho del nodo.
     NodoBinario<T>* obtenerHijoDer();
+
+    //  Establece el puntero al hijo izquierdo del nodo.
     void fijarHijoIzq(NodoBinario<T>* nodo);
+
+    //  Establece el puntero al hijo derecho del nodo.
     void fijarHijoDer(NodoBinario<T>* nodo);
 };
 
