2ca797390faf9ad498b8d1c4dbcfe98bc693ef94
XDavidF29 <140862157+XDavidF29@users.noreply.github.com>
2023-09-15 12:08:35 -0500

    NodoAVL.h

--- Trees/AVL/NodoAVL.h
+++ Trees/AVL/NodoAVL.h
@@ -7,26 +7,48 @@ using namespace std;
 template <class T>
 class NodoAVL
 {
-    protected:
-    T dato;
-    NodoAVL<T> *hijoIzq;
-    NodoAVL<T> *hijoDer;
+protected:
+    T dato;                   // Dato almacenado en el nodo
+    NodoAVL<T> *hijoIzq;     // Puntero al hijo izquierdo
+    NodoAVL<T> *hijoDer;     // Puntero al hijo derecho
+    int altura;              // Altura del subárbol con raíz en este nodo
 
-    int altura; // Altura del subárbol con raíz en este nodo
-
-    public:
+public:
+    // Constructor
     NodoAVL();
+
+    // Destructor
     ~NodoAVL();
+
+    // Métodos para establecer y obtener el dato del nodo
+
+    // Establece el valor de dato en el nodo.
     void fijarDato(const T& dato);
+
+    // Obtiene el valor almacenado en el nodo.
     const T& obtenerDato() const;
+
+    // Métodos para establecer y obtener los hijos del nodo
+
+    // Obtiene el puntero al hijo izquierdo del nodo.
     NodoAVL<T>* obtenerHijoIzq();
+
+    // Obtiene el puntero al hijo derecho del nodo.
     NodoAVL<T>* obtenerHijoDer();
+
+    // Establece el puntero al hijo izquierdo del nodo.
     void fijarHijoIzq(NodoAVL<T>* nodo);
+
+    // Establece el puntero al hijo derecho del nodo.
     void fijarHijoDer(NodoAVL<T>* nodo);
 
-    int obtenerAltura() const;
-    void actualizarAltura();
+    // Métodos para obtener y actualizar la altura del nodo
 
+    // Obtiene la altura del nodo.
+    int obtenerAltura() const;
+
+    // Actualiza la altura del nodo en función de la altura de sus hijos.
+    void actualizarAltura();
 };
 
 #include "NodoAVL.hxx"
