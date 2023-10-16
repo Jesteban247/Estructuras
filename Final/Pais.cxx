54d6338cbeea4827228b9a85bdb44a89e5a4f860
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2023-10-16 18:13:44 -0500

    Update Pais.cxx

--- Final/Pais.cxx
+++ Final/Pais.cxx
@@ -1,1 +1,43 @@
 
+class Pais
+{
+    private:
+        int id;
+        string continente;
+        string nombre;
+        int estado;
+
+    public:
+        Pais(int id, string continente, string nombre)
+        {
+            this->id = id;
+            this->continente = continente;
+            this->nombre = nombre;
+            this->estado = 0;
+        }
+
+        int getId()
+        {
+            return id;
+        }
+
+        string getContinente()
+        {
+            return continente;
+        }
+
+        string getNombre()
+        {
+            return nombre;
+        }
+
+        int getEstado()
+        {
+            return estado;
+        }
+
+        void setEstado(int estado)
+        {
+            this->estado = estado;
+        }
+};
