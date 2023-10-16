9dc69663561a5b2a68eea56c6162c582799d3524
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2023-10-16 18:22:06 -0500

    Update Pais.h

--- Final/Pais.h
+++ Final/Pais.h
@@ -1,1 +1,25 @@
+#ifndef Pais_H
+#define Pais_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pais
+{
+    private:
+        int id;
+        string continente;
+        string nombre;
+        int estado;
+
+    public:
+        Pais(int id, string continente, string nombre)
+        Pais(int id, string continente, string nombre)
+        getContinente();
+        getID();
+        getNombre();
+        getEstado();
+        setEstado();
+         
+};
