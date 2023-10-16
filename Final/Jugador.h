f75ca0ca2d900c4ac1de081fba0c23bb6dcfc2b8
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2023-10-16 18:37:18 -0500

    Update Jugador.h

--- Final/Jugador.h
+++ Final/Jugador.h
@@ -1,1 +1,39 @@
+#ifndef Jugador_H
+#define Jugador_H
 
+#include "Pais.h"
+#include <list>
+
+class Jugador
+{
+    private:
+        string nombre;
+        int turno;
+        int ejercitos;
+        //Guarda el id del pais y la cantidad de ejercitos del pais
+        map<int, int> paises;
+
+    public:
+        Jugador(string nombre, int turno, int ejercitos);
+        
+        string getNombre();
+
+        int getTurno();
+        
+        void setTurno(int turno);
+        
+        int getEjercitos();
+        
+        void setEjercitos(int ejercitos);
+
+        void agregarPais(int id, int ejercitos);
+      
+        map<int, int> getPaises();
+       
+        void setPaises(map<int, int> paises);
+
+        void EliminarPais(int id);
+       
+        void imprimirPaises();
+        
+};
